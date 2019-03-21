62d201a9e474729546f5d5e2daccd76f957641b2
Ashwini Desai <AshwiniDesai@SBMAC0326.local>
2019-03-21 17:30:48 +0530

    gMail attachment code and drop down code

---
+++ src/dropdown.java
@@ -0,0 +1,35 @@
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class dropdown {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.setProperty ("webdriver.chrome.driver","/Users/AshwiniDesai/Desktop/Selenium/Drivers/chromedriver");
+
+		WebDriver driver1 = new ChromeDriver();
+
+
+	driver1.get("https://www.google.com");
+	//To enter the keyword "Hello' in google search bar
+	driver1.findElement(By.name("q")).sendKeys("hello");
+	Thread.sleep(300);
+	
+	//To get all the list of values from the dropdown
+	List<WebElement> options=driver1.findElements(By.xpath("//ul[@role='listbox']/li")); 
+	int size1 = options.size();
+	System.out.println(size1);
+	
+	for (WebElement eachAnchorElem : options) {
+		
+        System.out.println(eachAnchorElem.getText());
+
+	}
+
+	}
+	}
---
+++ src/gMailBasics.java
@@ -0,0 +1,54 @@
+import java.awt.AWTException;
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class gMailBasics {
+
+	public static void main(String[] args) throws InterruptedException, AWTException, IOException {
+		
+		System.setProperty ("webdriver.chrome.driver","/Users/AshwiniDesai/Desktop/Selenium/Drivers/chromedriver");
+
+		WebDriver d1 = new ChromeDriver();
+		//WebDriver d2 = new ChromeDriver();
+
+
+	d1.get("https:www.gmail.com");
+	
+	// To enter the eMail and Password
+	d1.findElement(By.id("identifierId")).sendKeys("ashwini.sinchana27@gmail.com");
+	
+	d1.findElement(By.xpath("//span[.='Next']")).click();
+	Thread.sleep(5000);
+	d1.findElement(By.xpath("//*[@name='password']")).sendKeys("ashu@21sanja");
+	d1.findElement(By.xpath("//span[.='Next']")).click();
+	Thread.sleep(2000);
+	Thread.sleep(5000);
+	
+	// To click on the 'Compose' button
+	d1.findElement(By.xpath("//*[@id=':ik']/div/div")).click();
+	Thread.sleep(5000);
+	// To enter the 'To' address
+	d1.findElement(By.className("vO")).sendKeys("ashwini.sinchana27@gmail.com");
+	Thread.sleep(2000);
+	
+	//	To enter the 'Subject'
+	d1.findElement(By.name("subjectbox")).sendKeys("The beauty !!");
+	Thread.sleep(2000);
+	
+	//To click on the attachment icon
+	
+	d1.findElement(By.xpath("//*[@class='a1 aaA aMZ']")).click();
+	Thread.sleep(5000);
+	Runtime.getRuntime().exec("osascript " + "/Users/AshwiniDesai/Desktop/Attachment.scpt");
+	Thread.sleep(5000);
+	Thread.sleep(5000);
+	d1.findElement(By.xpath("//div[text()='Send']")).click();
+	
+	}
+
+}
